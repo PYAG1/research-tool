78660d6188d2743c2d8f20a5845f7ff2d393e742
PYAG <gyekyeyaw3@gmail.com>
2025-06-11 00:55:03 +0000

    feat upload to file flow
    - Got this flow from supabase UI. I just tweaked it a little to handle my use case. Would have to review it honestly

---
+++ src/renderer/src/components/shared/file/index.tsx
@@ -0,0 +1,35 @@
+
+import { useAuth } from "@renderer/context/auth";
+import Dropzone, {
+  DropzoneEmptyState,
+  DropzoneContent
+} from "react-dropzone/.";
+import { useSupabaseUpload } from "renderer/hooks/use-supabase-upload";
+
+export default function FileUpload({
+  onUploadComplete,
+  category_id,
+}: {
+  onUploadComplete?: () => void;
+  category_id?: string;
+}) {
+  const { session } = useAuth();
+  const props = useSupabaseUpload({
+    bucketName: "test",
+    path: session?.user.id ?? "default",
+    allowedMimeTypes: ["application/pdf"],
+    maxFiles: 1,
+    maxFileSize: 1000 * 1000 * 10, // 10MB,
+    onUploadComplete,
+    category_id: category_id,
+  });
+
+  return (
+    <div className="w-full">
+      <Dropzone {...props}>
+        <DropzoneEmptyState />
+        <DropzoneContent />
+      </Dropzone>
+    </div>
+  );
+}
